bc22292e959d11f98058d1b422e154f61a6f1d14
Vadim Papusha <137208219+VadimPY2143@users.noreply.github.com>
2024-12-12 20:27:14 +0200

    added practice code

--- library/urls.py
+++ library/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import book_list, search_books, add_book
 
 urlpatterns = [
-    path('',book_list, name='book_list'),
+    path('', book_list, name='book_list'),
     path('add_book/', add_book),
     path('search_books/', search_books, name='search_books'),
 ]
